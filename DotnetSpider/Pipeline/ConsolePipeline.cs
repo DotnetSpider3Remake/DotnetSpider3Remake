e4dac83dd29fe204ecd463ed08004b9a8b4403be
sanjusss <sanjusss@qq.com>
2020-03-07 09:45:13 +0800

    增加ConsolePipeline及其单元测试。

---
+++ DotnetSpider/Pipeline/ConsolePipeline.cs
@@ -0,0 +1,55 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotnetSpider.Pipeline
+{
+    public class ConsolePipeline : IPipeline
+    {
+        public ILog Logger { get; set; }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Task Process(IReadOnlyDictionary<string, object> resultItems, dynamic _ = null)
+        {
+            if (resultItems == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            foreach (var i in resultItems)
+            {
+                Console.WriteLine(i.Key + ":\t" + i.Value.ToString());
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Process(IEnumerable<IReadOnlyDictionary<string, object>> resultItems, dynamic _ = null)
+        {
+            if (resultItems == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            foreach (var i in resultItems)
+            {
+                if (i == null)
+                {
+                    continue;
+                }
+
+                foreach (var j in i)
+                {
+                    Console.WriteLine(j.Key + ":\t" + j.Value.ToString());
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DotnetSpiderTests/Pipeline/ConsolePipelineTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotnetSpider.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DotnetSpider.Pipeline.Tests
+{
+    [TestClass()]
+    public class ConsolePipelineTests
+    {
+        private readonly byte[] _data = new byte[4096];
+        private TextWriter _out = null;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _out = Console.Out;
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            Console.SetOut(_out);
+        }
+
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest0()
+        {
+            using (MemoryStream memory = new MemoryStream(_data))
+            {
+                using StreamWriter writer = new StreamWriter(memory);
+                Console.SetOut(writer);
+                using ConsolePipeline pipeline = new ConsolePipeline();
+                pipeline.Process(new Dictionary<string, object>()
+                {
+                    { "0", 0 }
+                });
+            }
+
+
+            using (MemoryStream memory = new MemoryStream(_data))
+            {
+                using StreamReader reader = new StreamReader(memory);
+                Assert.AreEqual("0:\t0", reader.ReadLine());
+            }
+        }
+
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest1()
+        {
+            using (MemoryStream memory = new MemoryStream(_data))
+            {
+                using StreamWriter writer = new StreamWriter(memory);
+                Console.SetOut(writer);
+                using ConsolePipeline pipeline = new ConsolePipeline();
+                pipeline.Process(new Dictionary<string, object>()
+                {
+                    { "0", 0 },
+                    { "1", 1 },
+                });
+            }
+
+
+            using (MemoryStream memory = new MemoryStream(_data))
+            {
+                using StreamReader reader = new StreamReader(memory);
+                Assert.AreEqual("0:\t0", reader.ReadLine());
+                Assert.AreEqual("1:\t1", reader.ReadLine());
+            }
+        }
+    }
+}
