7cd9170cd75d69fb7bec44498ec0491321af4020
sanjusss <sanjusss@qq.com>
2020-09-05 15:37:16 +0800

    测试用例完全覆盖Comparaor

--- DotnetSpiderTests/Common/ComparaorTests.cs
+++ DotnetSpiderTests/Common/ComparaorTests.cs
@@ -9,42 +9,125 @@ namespace DotnetSpider.Common.Tests
     [TestClass()]
     public class ComparaorTests
     {
-        #region AreEquivalent
+        #region AreEquivalentIDictionary
 
         [TestMethod()]
-        public void AreEquivalentTest0()
+        public void AreEquivalentIDictionaryTest0()
         {
             Assert.IsTrue(Comparaor.AreEquivalent<string, object>(null, null));
         }
 
         [TestMethod()]
-        public void AreEquivalentTest1()
+        public void AreEquivalentIDictionaryTest1()
         {
             Assert.IsFalse(Comparaor.AreEquivalent(null, new Dictionary<int, int>()));
         }
 
         [TestMethod()]
-        public void AreEquivalentTest2()
+        public void AreEquivalentIDictionaryTest2()
         {
             Assert.IsFalse(Comparaor.AreEquivalent(new Dictionary<int, int>(), null));
         }
 
         [TestMethod()]
-        public void AreEquivalentTest3()
+        public void AreEquivalentIDictionaryTest3()
         {
             Assert.IsTrue(Comparaor.AreEquivalent(new Dictionary<int, int>() { { 0, 0 } }, new Dictionary<int, int>() { { 0, 0 } }));
         }
 
         [TestMethod()]
-        public void AreEquivalentTest4()
+        public void AreEquivalentIDictionaryTest4()
         {
             Assert.IsTrue(Comparaor.AreEquivalent(new Dictionary<int, int>() { { 0, 0 } }, new SortedList<int, int>() { { 0, 0 } }));
         }
 
         [TestMethod()]
-        public void AreEquivalentTest5()
+        public void AreEquivalentIDictionaryTest5()
         {
             Assert.IsFalse(Comparaor.AreEquivalent(new Dictionary<int, int>() { { 0, 0 }, { 1, 1 } }, new Dictionary<int, int>() { { 0, 0 } }));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIDictionaryTest6()
+        {
+            var dic = new Dictionary<int, int>() { { 0, 0 } };
+            Assert.IsTrue(Comparaor.AreEquivalent(dic, dic));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIDictionaryTest7()
+        {
+            var a = new Dictionary<int, int>() { { 0, 0 } };
+            var b = new Dictionary<int, int>() { { 0, 1 } };
+            Assert.IsFalse(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIDictionaryTest8()
+        {
+            var a = new Dictionary<int, int>() { { 0, 0 } };
+            var b = new Dictionary<int, int>() { { 1, 0 } };
+            Assert.IsFalse(Comparaor.AreEquivalent(a, b));
+        }
+
+        #endregion
+
+        #region AreEquivalentIList
+
+        [TestMethod()]
+        public void AreEquivalentIListTest0()
+        {
+            List<int> a = new List<int> { };
+            List<int> b = new List<int> { };
+            Assert.IsTrue(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIListTest1()
+        {
+            List<int> a = null;
+            List<int> b = null;
+            Assert.IsTrue(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIListTest2()
+        {
+            List<int> a = new List<int> { };
+            List<int> b = null;
+            Assert.IsFalse(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIListTest3()
+        {
+            List<int> a = null;
+            List<int> b = new List<int> { };
+            Assert.IsFalse(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIListTest4()
+        {
+            List<int> a = new List<int> { };
+            List<int> b = a;
+            Assert.IsTrue(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIListTest5()
+        {
+            List<int> a = new List<int> { };
+            List<int> b = new List<int> { 1 };
+            Assert.IsFalse(Comparaor.AreEquivalent(a, b));
+        }
+
+        [TestMethod()]
+        public void AreEquivalentIListTest6()
+        {
+            List<int> a = new List<int> { 0, 0 };
+            List<int> b = new List<int> { 0, 1 };
+            Assert.IsFalse(Comparaor.AreEquivalent(a, b));
         }
 
         #endregion
