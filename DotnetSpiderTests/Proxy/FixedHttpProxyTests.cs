c53a828319442bb5a23fe24310313ade74c41c6d
sanjusss <sanjusss@qq.com>
2020-09-05 17:02:27 +0800

    完全覆盖FixedHttpProxy

--- DotnetSpiderTests/Proxy/FixedHttpProxyTests.cs
+++ DotnetSpiderTests/Proxy/FixedHttpProxyTests.cs
@@ -29,5 +29,26 @@ namespace DotnetSpider.Proxy.Tests
             await fixedHttpProxy.ReturnProxy(null, null);
             Assert.AreSame(proxy, await fixedHttpProxy.GetProxy(null));
         }
+
+        [TestMethod()]
+        public async Task FixedHttpProxyTest()
+        {
+            Fakes.StubFixedHttpProxy fixedHttpProxy = new Fakes.StubFixedHttpProxy()
+            {
+                CallBase = true
+            };
+            Assert.IsNull(await fixedHttpProxy.GetProxy(null));
+        }
+
+        [TestMethod()]
+        public void PropertyTest()
+        {
+            FixedHttpProxy fixedHttpProxy = new FixedHttpProxy(null);
+            log4net.Fakes.StubILog log = new log4net.Fakes.StubILog();
+            fixedHttpProxy.Logger = log;
+            Assert.AreSame(log, fixedHttpProxy.Logger);
+            fixedHttpProxy.Name = "name";
+            Assert.AreEqual("name", fixedHttpProxy.Name);
+        }
     }
 }
