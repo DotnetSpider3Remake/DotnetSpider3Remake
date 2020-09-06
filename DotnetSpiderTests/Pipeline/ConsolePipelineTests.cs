e4da48707c1c944a16f2a40a2560986f256878e8
sanjusss <sanjusss@qq.com>
2020-09-06 10:09:12 +0800

    测试用例覆盖ConsolePipeline

--- DotnetSpiderTests/Pipeline/ConsolePipelineTests.cs
+++ DotnetSpiderTests/Pipeline/ConsolePipelineTests.cs
@@ -4,73 +4,178 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.QualityTools.Testing.Fakes;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
 
 namespace DotnetSpider.Pipeline.Tests
 {
+    [ExcludeFromCodeCoverage]
     [TestClass()]
     public class ConsolePipelineTests
     {
-        private readonly byte[] _data = new byte[4096];
-        private TextWriter _out = null;
+        private IDisposable _shimsContext = null;
+        private string _output = string.Empty;
 
         [TestInitialize]
         public void Init()
         {
-            _out = Console.Out;
+            _shimsContext = ShimsContext.Create();
+            System.Fakes.ShimConsole.WriteLineString = s => _output += s + "\n";
         }
 
         [TestCleanup]
         public void Clean()
         {
-            Console.SetOut(_out);
+            _shimsContext.Dispose();
+            _shimsContext = null;
+            _output = string.Empty;
+        }
+
+        [TestMethod()]
+        [Timeout(5000)]
+        public void PropertyTest()
+        {
+            using ConsolePipeline pipeline = new ConsolePipeline();
+            log4net.Fakes.StubILog log = new log4net.Fakes.StubILog();
+            pipeline.Logger = log;
+            Assert.AreSame(log, pipeline.Logger);
+            pipeline.Name = "name";
+            Assert.AreEqual("name", pipeline.Name);
+            Assert.IsFalse(pipeline.IsCacheSupported);
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                bool _ = pipeline.EnableCache;
+            });
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                pipeline.EnableCache = true;
+            });
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                var _ = pipeline.MaxCacheTime;
+            });
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                pipeline.MaxCacheTime = Timeout.InfiniteTimeSpan;
+            });
         }
 
         [TestMethod()]
         [Timeout(5000)]
         public void ProcessTest0()
         {
-            using (MemoryStream memory = new MemoryStream(_data))
-            {
-                using StreamWriter writer = new StreamWriter(memory);
-                Console.SetOut(writer);
-                using ConsolePipeline pipeline = new ConsolePipeline();
-                pipeline.Process(new Dictionary<string, object>()
+            using ConsolePipeline pipeline = new ConsolePipeline();
+            pipeline.Process(new Dictionary<string, object>()
                 {
                     { "0", 0 }
                 });
-            }
-
-
-            using (MemoryStream memory = new MemoryStream(_data))
-            {
-                using StreamReader reader = new StreamReader(memory);
-                Assert.AreEqual("0:\t0", reader.ReadLine());
-            }
+            Assert.AreEqual("0:\t0\n", _output);
         }
 
         [TestMethod()]
         [Timeout(5000)]
         public void ProcessTest1()
         {
-            using (MemoryStream memory = new MemoryStream(_data))
+            bool printLog = false;
+            ILog log = new log4net.Fakes.StubILog
             {
-                using StreamWriter writer = new StreamWriter(memory);
-                Console.SetOut(writer);
-                using ConsolePipeline pipeline = new ConsolePipeline();
-                pipeline.Process(new Dictionary<string, object>()
+                InfoObject = _ => printLog = true
+            };
+            using ConsolePipeline pipeline = new ConsolePipeline
+            {
+                Logger = log
+            };
+            pipeline.Process(new Dictionary<string, object>()
                 {
                     { "0", 0 },
                     { "1", 1 },
                 });
-            }
+            Assert.IsTrue(printLog);
+            string[] lines = _output.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            Assert.AreEqual(2, lines.Length);
+            Assert.AreEqual("0:\t0", lines[0]);
+            Assert.AreEqual("1:\t1", lines[1]);
+        }
 
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest2()
+        {
+            using ConsolePipeline pipeline = new ConsolePipeline();
+            var task = pipeline.Process((IReadOnlyDictionary<string, object>)null);
+            Assert.AreSame(Task.CompletedTask, task);
+        }
 
-            using (MemoryStream memory = new MemoryStream(_data))
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest3()
+        {
+            using ConsolePipeline pipeline = new ConsolePipeline();
+            var task = pipeline.Process((IEnumerable<IReadOnlyDictionary<string, object>>)null);
+            Assert.AreSame(Task.CompletedTask, task);
+        }
+
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest4()
+        {
+            bool printLog = false;
+            ILog log = new log4net.Fakes.StubILog
             {
-                using StreamReader reader = new StreamReader(memory);
-                Assert.AreEqual("0:\t0", reader.ReadLine());
-                Assert.AreEqual("1:\t1", reader.ReadLine());
-            }
+                InfoObject = _ => printLog = true
+            };
+            using ConsolePipeline pipeline = new ConsolePipeline
+            {
+                Logger = log
+            };
+            var results = new List<IReadOnlyDictionary<string, object>>();
+            pipeline.Process(results);
+            Assert.IsFalse(printLog);
+        }
+
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest5()
+        {
+            bool printLog = false;
+            ILog log = new log4net.Fakes.StubILog
+            {
+                InfoObject = _ => printLog = true
+            };
+            using ConsolePipeline pipeline = new ConsolePipeline
+            {
+                Logger = log
+            };
+            var results = new List<IReadOnlyDictionary<string, object>>
+            {
+                new Dictionary<string, object>()
+                {
+                    { "0", 0 }
+                }
+            };
+            pipeline.Process(results);
+            Assert.AreEqual("0:\t0\n", _output);
+            Assert.IsTrue(printLog);
+        }
+
+        [TestMethod()]
+        [Timeout(5000)]
+        public void ProcessTest6()
+        {
+            using ConsolePipeline pipeline = new ConsolePipeline();
+            var results = new List<IReadOnlyDictionary<string, object>>
+            {
+                new Dictionary<string, object>()
+                {
+                    { "0", 0 }
+                },
+                null
+            };
+            pipeline.Process(results);
+            Assert.AreEqual("0:\t0\n", _output);
         }
     }
 }
